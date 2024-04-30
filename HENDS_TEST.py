33d42e0fae3c33d734a5824a07955551763c0a31
Almaz Muzafarov <muzafrov.almaz2012@yandex.ru>
2024-04-30 23:18:48 +0400

    Children classes Smartphone and grass, #isinstance, #super()

--- HENDS_TEST.py
+++ HENDS_TEST.py
@@ -1,7 +1,7 @@
 #Добавлено специально для тестированияя правильности работы,
 # что-то вроде модуля ручного тестирования, для личного пользования и илюстрации проблемы
 
-from classes import Category, Product
+from classes import Category, Product, Smartphone
 
 tools = Category("Инструменты", "Auto repair tools", [])
 
@@ -13,18 +13,22 @@ product2 = Product("ключ", "14 mm", 235.99, 115)
 product2.price = 250
 print(product2)
 
-
 tools.add_product_at_list = product1
 tools.add_product_at_list = product2
-#print(tools.return_product)
+# print(tools.return_product)
 
 print(tools)
 print(product1 + product2)
-#В данном случае выводится
-#насос, 2050 руб. Остаток: 30 шт.
-#ключ, 250 руб. Остаток: 115 шт.
-#Инструменты, количество продуктов: 30 шт. - должно быть не 30 штук, а 145
-#Это подзадание первого задания
-#90250
+# В данном случае выводится
+# насос, 2050 руб. Остаток: 30 шт.
+# ключ, 250 руб. Остаток: 115 шт.
+# Инструменты, количество продуктов: 30 шт. - должно быть не 30 штук, а 145
+# Это подзадание первого задания
+# 90250
 print(product1 + product2)
+print(type(product1))
+product3 = Smartphone("Сяёми", "2024", 15000, 115, 50000, 'Y11', '128Gb', 'Gray')
 
+print(product3)
+print(type(product3))
+print(product1 + product3)
--- classes.py
+++ classes.py
@@ -34,8 +34,12 @@ class Category:
     @add_product_at_list.setter
     def add_product_at_list(self, object_product):
         """Внесение продукта(товара) в список"""
-        self.products.append(object_product)
-        Category.quantity_products += 1
+        if isinstance(object_product, Product):
+            self.products.append(object_product)
+            Category.quantity_products += 1
+        else:
+            raise TypeError(
+                "Можно добавлять только обекты класса 'Product' или обекты дочерних классов класса 'Product'")
 
     @property
     def return_product(self):
@@ -79,6 +83,32 @@ class Product:
             self.__price = new_price
 
     def __add__(self, other):
-        """Метод выполняется при сложении двух объектов класса 'Product'и
+        """Метод выполняется при сложении двух объектов одного класса(Нельзя сложить объект родительского и дочернего класса) и
         вернет сумму произведений количества на цену двух этих объектов"""
-        return self.price * self.quantity_in_stock + other.price * other.quantity_in_stock
+        if type(self) == type(other):
+            return self.price * self.quantity_in_stock + other.price * other.quantity_in_stock
+        else:
+            raise TypeError
+
+
+class Smartphone(Product):
+    """Дочерний класс класса Продукт"""
+
+    def __init__(self, name: str, discription: str, price: float, quantity_in_stock: int, performance, model, memory,
+                 color):
+        super().__init__(name, discription, price, quantity_in_stock)
+        self.performance = performance
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class Grass(Product):
+    """Дочерний класс класса Продукт"""
+
+    def __init__(self, name: str, discription: str, price: float, quantity_in_stock: int, country, germination_period,
+                 color):
+        super().__init__(name, discription, price, quantity_in_stock)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
